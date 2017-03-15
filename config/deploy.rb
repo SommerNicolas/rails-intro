d5ac62bbd06a7da1bf7bcb2e029d4f41c92675bd
SommerNicolas <nicolas.sommer@he-arc.ch>
2017-03-15 14:59:13 +0100

    deploy ready

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.8.0"
 
 set :application, "rails-intro"
-set :repo_url, "git@github.com/SommerNicolas/rails-intro.git"
+set :repo_url, "git@github.com:SommerNicolas/rails-intro.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
